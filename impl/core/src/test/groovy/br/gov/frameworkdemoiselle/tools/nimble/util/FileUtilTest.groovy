2d27c0b7eb2cde7816063534c5bae7243b946a30
esaito <emerson.saito@gmail.com>
2012-09-14 12:05:05 -0300

    0000647: Selecionar artefato do Projeto - CRUD 0000810: [Template-CRUD] Tipo do campo ID Fixo

--- impl/core/src/test/groovy/br/gov/frameworkdemoiselle/tools/nimble/util/FileUtilTest.groovy
+++ impl/core/src/test/groovy/br/gov/frameworkdemoiselle/tools/nimble/util/FileUtilTest.groovy
@@ -85,5 +85,128 @@ class FileUtilTest extends GroovyTestCase {
 		def expected = [var1:"value1", var2:"value2", var3:"value3"]
 		assertTrue expected == FileUtil.convertPropsToMap("./src/test/vars.txt")
 	}
+
+	String code1 = '''
+	package br.org.frameworkdemoiselle.TesteNimble.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@MappedSuperclass
+public abstract class Pessoa {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private Long cpf;
+
+	@Column(nullable = false, length = 255)
+	private String nome;
+
+	@Column(nullable = false)
+	@Temporal(value = TemporalType.DATE)
+	private Date dataNascimento;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+}
+		'''
+			
+			
+			def code2 = '''
+		@Entity
+public class Classe2 extends Classe1 implements Serializable {
+
+	/**
+		 *
+		 */
+	private static final long serialVersionUID = 1L;
+
+	@Column(nullable = false)
+	@Temporal(value = TemporalType.DATE)
+	private Date dataMatricula;
+
+	@Column
+	private int numeroMatricula;
+
+	public Date getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(Date dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
+
+	public void setNumeroMatricula(int numeroMatricula) {
+		this.numeroMatricula = numeroMatricula;
+	}
+
+}
+		'''
+	void testHasString() {
+		
+		def tempDir = new File("./src/test/temp/")
+		if (tempDir.exists()) tempDir.deleteDir()
+		tempDir.mkdir()
+		
+		def tmpFile1 = new File("./src/test/temp/Classe1.java")
+		tmpFile1 << code1
+		
+		def tmpFile2 = new File("./src/test/temp/Classe2.java")
+		tmpFile2 << code2
+		
+		def expected = true
+		
+		assert expected == FileUtil.hasString ("./src/test/temp/Classe2.java", "@Entity")
+						
+		assert expected == FileUtil.hasString ("./src/test/temp/Classe1.java", "@Id")
+		
+		expected = false
+		
+		assert expected == FileUtil.hasString ("./src/test/temp/Classe2.java", "@Id")
+		
+		assert expected == FileUtil.hasString ("./src/test/temp/Classe1.java", "@Entity")
+	}
 	
+		
 }
--- impl/core/src/test/groovy/br/gov/frameworkdemoiselle/tools/nimble/util/RegexUtilTest.groovy
+++ impl/core/src/test/groovy/br/gov/frameworkdemoiselle/tools/nimble/util/RegexUtilTest.groovy
@@ -1,9 +1,16 @@
 package br.gov.frameworkdemoiselle.tools.nimble.util
 
-import br.gov.frameworkdemoiselle.tools.nimble.util.RegexUtil;
+import java.io.Serializable;
+import java.util.Date;
 
 
 public class RegexUtilTest extends GroovyTestCase {
+	
+	void tearDown() {
+		File tempDir = new File("./src/test/temp/")
+		if (tempDir.exists())
+			FileUtil.delTree(tempDir)
+	}
 	
 	void testGetClassAttributes() {
 		def code = '''
@@ -38,7 +45,7 @@ public class RegexUtilTest extends GroovyTestCase {
 		
 		assert expected == RegexUtil.getClassAttributes(code)
 	}
-	
+
 	void testGetClassAttributesFromFile() {
 		def code = '''
 		class A {
@@ -76,4 +83,109 @@ public class RegexUtilTest extends GroovyTestCase {
 		
 		assert expected == RegexUtil.getClassAttributesFromFile(tmpFile.path)
 	}
+	
+	
+	void testGetClassAttributesFromFileAndPath() {
+		def code1 = '''
+	@MappedSuperclass
+public abstract class Classe1 {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column
+	private Long cpf;
+
+	@Column(nullable = false, length = 255)
+	private String nome;
+
+	@Column(nullable = false)
+	@Temporal(value = TemporalType.DATE)
+	private Date dataNascimento;
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+}
+		'''
+		
+		
+		def code2 = '''
+		@Entity
+public class Classe2 extends Classe1 implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Column(nullable = false)
+	@Temporal(value = TemporalType.DATE)
+	private Date dataMatricula;
+
+	@Column
+	private int numeroMatricula;
+
+	public Date getDataMatricula() {
+		return dataMatricula;
+	}
+
+	public void setDataMatricula(Date dataMatricula) {
+		this.dataMatricula = dataMatricula;
+	}
+
+	public int getNumeroMatricula() {
+		return numeroMatricula;
+	}
+
+	public void setNumeroMatricula(int numeroMatricula) {
+		this.numeroMatricula = numeroMatricula;
+	}
+
+}
+		'''
+		
+		def tempDir = new File("./src/test/temp/")
+		if (tempDir.exists()) tempDir.deleteDir()
+		tempDir.mkdir()
+		def tmpFile1 = new File("./src/test/temp/Classe1.java")
+		tmpFile1 << code1
+		
+		def tmpFile2 = new File("./src/test/temp/Classe2.java")
+		tmpFile2 << code2
+		
+		def expected = ['A':'String','BLong':'Long','Cint':'int','DBoolean':'boolean','DataMatricula':'Date','NumeroMatricula':'int','Id':'Long','Nome':'String','Cpf':'Long','DataNascimento':'Date']
+		
+		assert expected == RegexUtil.getClassAttributesFromFile(tmpFile2.getName(),tmpFile2.getParent()+"/" )
+	}
+	
 }
