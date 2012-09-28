79f7a9a8489575f80057f3838e4ca27b22a83f0f
esaito <emerson.saito@gmail.com>
2012-09-28 18:19:40 -0300

    0000810: [Template-CRUD] Tipo do campo ID Fixo 0000813: Criação de template CRUD para mobile com PrimeFace3.

--- impl/core/src/main/templates/generate-crud-jsf-prime3/src/main/resources/messages.properties.fragment.groovy
+++ impl/core/src/main/templates/generate-crud-jsf-prime3/src/main/resources/messages.properties.fragment.groovy
@@ -1,8 +1,9 @@
 menu.${beanLower}=${pojo}s
 ${beanLower}.label=${pojo}
 <% 
-import br.gov.frameworkdemoiselle.tools.nimble.util.RegexUtil as RU
-def attrList = RU.getClassAttributesFromFile(beanJavaName, beanPath)
+import br.gov.frameworkdemoiselle.tools.nimble.util.ReflectionUtil as RU
+def tmpFile = new File(beanPath+beanJavaName)
+def attrList = RU.getAttributesFromClassFile(tmpFile)
 %>
 ${beanLower}-delete-ok=${pojo} exclu\\u00EDdo: {0}
 ${beanLower}-insert-ok=${pojo} inserido: {0}
